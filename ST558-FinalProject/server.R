01507950de5e02cd3b08fc323fd5405003a63c71
Vito Frank Leonardo <VitoFrankLeonardo@gmail.com>
2021-08-02 16:21:52 -0400

    Data

--- ST558-FinalProject/server.R
+++ ST558-FinalProject/server.R
@@ -8,21 +8,55 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(readxl)
 
+Credit <- read_excel(path = "default of credit card clients.xls",
+                     col_names = TRUE, 
+                     skip=1) %>% 
+    rename(DEFAULT = `default payment next month`) %>%
+    mutate(DEFAULT = factor(DEFAULT, levels = c(1,0), labels = c("Yes", "No")),
+           SEX = factor(SEX, levels = c(1,2), labels = c("Male", "Female")),
+           EDUCATION = factor(EDUCATION, levels = c(1,2,3,4), 
+                              labels = c('Graduate School', 'University', 'High School', 'others')),
+           MARRIAGE = factor(MARRIAGE, levels = c(1,2,3), 
+                             labels = c('Married', 'Single', 'Others'))) %>%
+    select(-ID) %>% drop_na()
 
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+shinyServer(function(input, output, session) {
+    
+    tableData <- reactive({
+        if(input$Subset){
+            varlist1 <- input$var1
+            if("Status" %in% varlist1){
+                varlist1[[which.max("Status" == varlist1 )]] <- 6:11
+            }
+            if("Amount" %in% varlist1){
+                varlist1[[which.max("Amount" == varlist1 )]] <- 18:23
+            }
+            if("Bill" %in% varlist1){
+                varlist1[[which.max("Bill" == varlist1 )]] <- 12:17
+            }
+            varlist1 <- c(as.numeric(unlist(varlist1)),24)
+            tableData <- Credit %>% select(varlist1)
+        } else tableData <- Credit
+        if(input$Def){
+            tableData <- tableData %>% filter(DEFAULT %in% input$Def) 
+        } 
     })
+    
+    output$table <- renderDataTable({
+        datatable(tableData(),
+                      options = list(scrollX = TRUE))
+    })
+
+ 
+    
+    
+    
+    
+    
 
 })
--- ST558-FinalProject/ui.R
+++ ST558-FinalProject/ui.R
@@ -9,21 +9,104 @@
 
 library(shiny)
 library(markdown)
+library(tidyverse)
+library(readxl)
+library(DT)
 
+Credit <- read_excel(path = "default of credit card clients.xls",
+                     col_names = TRUE, 
+                     skip=1) %>% 
+  rename(DEFAULT = `default payment next month`) %>%
+  mutate(DEFAULT = factor(DEFAULT, levels = c(1,0), labels = c("Yes", "No")),
+         SEX = factor(SEX, levels = c(1,2), labels = c("Male", "Female")),
+         EDUCATION = factor(EDUCATION, levels = c(1,2,3,4), 
+                            labels = c('Graduate School', 'University', 'High School', 'others')),
+         MARRIAGE = factor(MARRIAGE, levels = c(1,2,3), 
+                           labels = c('Married', 'Single', 'Others'))) %>%
+  select(-ID) %>% drop_na()
 
 # Define UI for application that draws a histogram
 shinyUI(navbarPage("ST558 Final Project",
+                   
+                   
+                   
+                   
+                   
                    tabPanel("About",
                             fluidRow(
                                 column(6,
                                        includeMarkdown("about.md")
                                 ),
                                 column(3,
-                                       img(src="creditriskcollage.jpg")
+                                       img(src="creditriskcollage.jpg",
+                                           width = 510,
+                                           height =510)
                                        )
                                 )
                             ),
-                   tabPanel("Data"),
+                   
+                   
+                   
+                   
+                   
+                   tabPanel("Data",
+                            sidebarLayout(
+                              sidebarPanel(
+                                checkboxInput("Subset", "Select a Subset?"),
+                                
+                                conditionalPanel(
+                                  condition = "input.Subset",
+                                  checkboxGroupInput(inputId = "var1", label = "Variables", 
+                                              choiceNames = c("Credit Limit", 
+                                                          "Gender", 
+                                                          "Education",
+                                                          "Marriage",
+                                                          "Age",
+                                                          "Past Payment Status",
+                                                          "Past Payment Amount",
+                                                          "Bill Statement History"
+                                                          ),
+                                              choiceValues = list(1, 
+                                                               2, 
+                                                               3,
+                                                               4,
+                                                               5,
+                                                               'Status',
+                                                               'Amount',
+                                                               'Bill'
+                                                               ),
+                                              selected = c("Credit Limit", 
+                                                           "Gender", 
+                                                           "Education",
+                                                           "Marriage",
+                                                           "Age",
+                                                           "Past Payment Status",
+                                                           "Past Payment Amount",
+                                                           "Bill Statement History"
+                                              )
+                                  )
+                                ),
+                                checkboxInput("Filter", "Filter Data?"),
+                                
+                                conditionalPanel(
+                                  condition = "input.Filter",
+                                  selectInput(inputId = "Def", label = "Default", 
+                                              choices = c("Yes", "No"))
+                                )
+                              ),
+                                
+                                
+                              mainPanel(
+                                dataTableOutput("table")
+                              )
+                            )
+                   ),
+                   
+                   
+                   
+                   
+                   
+                   
                    tabPanel("Data Exploration"),
                    navbarMenu("Modeling",
                               tabPanel("Modeling Info"),
